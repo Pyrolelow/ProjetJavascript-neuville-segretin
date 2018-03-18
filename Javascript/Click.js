212a845f3d9fda76bb58cbacf098df8e390b56aa
Pyrolelow <alexandre.segretin@gmail.com>
2018-03-18 15:23:59 +0100

    Marteau
    Ajout du changement du prix du marteau et combien on gagne à chaque clique.

--- Javascript/Click.js
+++ Javascript/Click.js
@@ -3,32 +3,51 @@
 $('document').ready(function() {
    
   let marteau = $("#marteau");
-
+  var spanPrixMarteur = $("#prixMarteau");
+  let titre = $("title");
+  console.log(spanPrixMarteur);
   var nombreDeFer = 0;
   var nbMarteau = 0;
-
+  var Ferparclick = 1;
+  var PrixMarteau = 10;
   var achatMart = document.getElementById('nbMarteau');
   var image = document.getElementById("enclume");
   var resultat = document.getElementById("Indicateur");
 
+  
+  document.onload = function () { 
+      afficherScore();
+      afficherPrixMarteau();
+  }
+
+
+
   function afficherScore () {
     resultat.innerHTML = "Ressources : " + nombreDeFer;
+    titre.text(nombreDeFer + " Fer");
+  }
+
+  function afficherPrixMarteau (){
+    spanPrixMarteur.text(PrixMarteau);
   }
  
 
   marteau.click( function () {
-    if(nombreDeFer >= 10) {
+    if(nombreDeFer >= PrixMarteau) {
       nbMarteau = nbMarteau + 1;
       achatMart.innerHTML = nbMarteau;
-      nombreDeFer = nombreDeFer - 10;
+      nombreDeFer = nombreDeFer - PrixMarteau;
+      PrixMarteau = PrixMarteau*3;
+      Ferparclick = Ferparclick * 2;
       afficherScore();
+      afficherPrixMarteau();
     }else{
       alert("Continuez vos efforts !");
     }
   });
 
   image.addEventListener("click", function () {
-      nombreDeFer = nombreDeFer + 1;
+      nombreDeFer = nombreDeFer + Ferparclick;
       afficherScore();
   });
 
