c8aa2da031a73b70c7096edea9bc5bda86b5b931
Thomas Neuville <thomas.neuville@etudiant.univ-lr.fr>
2018-03-18 14:36:20 +0100

    propre

--- Javascript/Click.js
+++ Javascript/Click.js
@@ -6,15 +6,8 @@ $('document').ready(function() {
 
   var nombreDeFer = 0;
   var nbMarteau = 0;
-  
-  console.log(marteau);
-  var achatMart = document.getElementById('nbMarteau');
-  /*var marteau = $('tr:gt(0)').find('td:first');
-  var guerrier=$('tr:gt(0)').find('td').eq(1);
-  var mineur=$('tr:gt(0)').find('td').eq(2);
-  var forgeron=$('tr:gt(0)').find('td').eq(3);
-  var forge=$('tr:gt(0)').find('td:get(3)');*/
 
+  var achatMart = document.getElementById('nbMarteau');
   var image = document.getElementById("enclume");
   var resultat = document.getElementById("Indicateur");
 
