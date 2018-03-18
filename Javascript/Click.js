7ca17e07630cada6f20634119dc48e00d9254929
Thomas Neuville <thomas.neuville@etudiant.univ-lr.fr>
2018-03-18 15:20:06 +0100

    ajout forgeron

--- Javascript/Click.js
+++ Javascript/Click.js
@@ -2,25 +2,50 @@
 
 $('document').ready(function() {
    
-  let marteau = $("#marteau");
+  baseInterval = 1000;
 
-  var nombreDeFer = 0;
-  var nbMarteau = 0;
-
-  var achatMart = document.getElementById('nbMarteau');
   var image = document.getElementById("enclume");
   var resultat = document.getElementById("Indicateur");
+
+  var nombreDeFer = 0;
+
+  var nbMarteau = 0;
+  let marteau = $("#marteau");
+  var achatMart = document.getElementById('nbMarteau');
+  
+  var nbForgeron = 0;
+  var autoForgeron = 0;
+  let forgeron = $("#forgeron");
+  var achatForgeron = document.getElementById('nbForgeron');
+
 
   function afficherScore () {
     resultat.innerHTML = "Ressources : " + nombreDeFer;
   }
  
+ function update () {
+  console.log('update');
+  nombreDeFer += autoForgeron;
+  afficherScore();
+ }
 
   marteau.click( function () {
     if(nombreDeFer >= 10) {
-      nbMarteau = nbMarteau + 1;
+      nbMarteau++;
       achatMart.innerHTML = nbMarteau;
-      nombreDeFer = nombreDeFer - 10;
+      nombreDeFer -= 10;
+      afficherScore();
+    }else{
+      alert("Continuez vos efforts !");
+    }
+  });
+
+  forgeron.click( function () {
+    if(nombreDeFer >= 1500) {
+      nbForgeron++;
+      autoForgeron += 1;
+      achatForgeron.innerHTML = nbForgeron;
+      nombreDeFer -= 1500;
       afficherScore();
     }else{
       alert("Continuez vos efforts !");
@@ -31,5 +56,9 @@ $('document').ready(function() {
       nombreDeFer = nombreDeFer + 1;
       afficherScore();
   });
+
+  window.setInterval( function() {
+    update();
+  },baseInterval);
 
 });
