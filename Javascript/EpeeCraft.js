9b2f1a49119783888d83bd0643fcac9878ffe52c
Thomas Neuville <thomas.neuville@etudiant.univ-lr.fr>
2018-03-23 00:02:38 +0100

    ajout son

--- Javascript/EpeeCraft.js
+++ Javascript/EpeeCraft.js
@@ -5,9 +5,10 @@ $(document).ready(function(){
 	var armeEquipement = $(".epee");
 
 	epeeBois.click(function(){
-		console.log(nombreDeBois,nombreDeCuir,nombreDeFer);
 		if (epeeBoisPrix[0] <= nombreDeBois && epeeBoisPrix[1] <= nombreDeCuir 
 			&& epeeBoisPrix[2] <= nombreDeFer){
+			let audio = new Audio("Sons/sonEnclume.mp3");
+    		audio.play();
 			armeEquipement.attr('src', "images/epeeboisactive.png");
 			nombreDeBois = nombreDeBois - epeeBoisPrix[0];
 			nombreDeCuir = nombreDeCuir - epeeBoisPrix[1];
@@ -15,5 +16,6 @@ $(document).ready(function(){
 		}else{
 			alert("Il vous faut plus de ressources pour fabriquer votre épée en bois.");
 		}
-	})
+	});
+
 });
