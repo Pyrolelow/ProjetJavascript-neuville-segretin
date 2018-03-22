b1b8b583bf6a47f3cecb23575de340336b8b3bed
Thomas Neuville <thomas.neuville@etudiant.univ-lr.fr>
2018-03-22 01:25:28 +0100

    ajout interface personnage (casque,épée,bouclier,haut,bas,botte

--- Javascript/craft.js
+++ Javascript/craft.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
 
-var epeeBois = $("#epeeEnBois");
-var epeeBoisPrix = [200,100,0];
-var armeEquipement = $("#armeActuelle")
+	var epeeBois = $("#epeeEnBois");
+	var epeeBoisPrix = [1,1,0];
+	var armeEquipement = $(".epee");
 
-epeeBois.click(function(){
-    console.log(nombreDeBois,nombreDeCuir,nombreDeFer);
-if (epeeBoisPrix[0] <= nombreDeBois && epeeBoisPrix[1] <= nombreDeCuir 
-&& epeeBoisPrix[2] <= nombreDeFer){
-    armeEquipement.html('<img id="epeeEnBois" src="images/epee_en_bois.png"/>');
-}else{
-    alert("Il vous faut plus de ressources pour fabriquer votre épée en bois.");
-}
-})
-
+	epeeBois.click(function(){
+		console.log(nombreDeBois,nombreDeCuir,nombreDeFer);
+		if (epeeBoisPrix[0] <= nombreDeBois && epeeBoisPrix[1] <= nombreDeCuir 
+			&& epeeBoisPrix[2] <= nombreDeFer){
+			armeEquipement.attr('src', "images/epeeboisactive.png");
+		}else{
+			alert("Il vous faut plus de ressources pour fabriquer votre épée en bois.");
+		}
+	})
 });
