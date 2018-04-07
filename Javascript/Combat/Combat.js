7713b4d0e091b584760c5d856acb0b66be5ef409
Pyrolelow <alexandre.segretin@gmail.com>
2018-04-07 14:16:45 +0200

    Barre de vie OP

--- Javascript/Combat/Combat.js
+++ Javascript/Combat/Combat.js
@@ -1,23 +1,21 @@
 var combat;
 var hero;
+var monstre;
 
 $('document').ready(function(){
     
     hero = new Personnage();
-    var monstre = new Monstre();
+    monstre = new Monstre();
     var vieRestanteMonstre;
     var vieRestanteHero;
 
     combat = function(){
-    console.log(hero.dommage);
-    if(hero.hp > 0){
+    if(hero.hp > 0 && monstre.hp > 0){
+        
         vieRestanteMonstre = monstre.hp - hero.dommage;
         console.log("Vie du monstre :" + vieRestanteMonstre);
         monstre.hp = vieRestanteMonstre;
-    }else if(hero.hp <= 0){
-        hero.hp = 100 * hero.level;
-        monstre.hp = 100 * hero.level; 
-    }
+    
 
     if(hero.experiencePourUp <= hero.experienceActuel){
         hero.experienceActuel = 0;
@@ -26,7 +24,7 @@ $('document').ready(function(){
         console.log("Level up ! Niveaux " + hero.level);
     }
 
-    if(monstre.hp > 0){
+    
         if((monstre.dmg - hero.armure) > 0){
             vieRestanteHero = hero.hp - (monstre.dmg - hero.armure);
             console.log("Votre vie :" + vieRestanteHero);
@@ -34,6 +32,11 @@ $('document').ready(function(){
         }else{
             console.log("Dommages bloqués !");
         }
+    
+    
+    }else if(hero.hp <= 0){
+        hero.hp = 100 * hero.level;
+        monstre.hp = 100 * hero.level; 
     }else if(monstre.hp <= 0){
         hero.experienceActuel += monstre.experience;
         monstre.hp = 100*hero.level;
--- Javascript/Combat/CombatCanvas.js
+++ Javascript/Combat/CombatCanvas.js
@@ -5,17 +5,19 @@ $('body').ready(function(){
     let combatBackground = document.getElementById('background');
     let ennemi = document.getElementById('ennemi');
     let personnage = document.getElementById('personnage');
+    let cent = 350;
 
     combatBackground.onload = function(){
-        
-        ctx.drawImage(combatBackground,0,0,1280,720);
-        
+        window.setInterval( function() {
 
+        ctx.clearRect(0, 0, 1280, 720);
+
+        ctx.drawImage(combatBackground,0,0,1280,720);
+        ctx.drawImage(ennemi,935,320,220,300)
+
+        ctx.drawImage(personnage,120,330,200,250)
 
         ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(70, 70, 350, 20);
-
-        ctx.fillStyle = 'rgb(0, 200, 0)';
         ctx.fillRect(70, 70, 350, 20);
 
         ctx.fillStyle = 'black';
@@ -24,18 +26,45 @@ $('body').ready(function(){
         ctx.fillStyle = 'rgb(200, 0, 0)';
         ctx.fillRect(850, 70, 350, 20);
 
-        ctx.fillStyle = 'rgb(0, 200, 0)';
-        ctx.fillRect(850, 70, 350, 20);
-
         ctx.fillStyle = 'black';
         ctx.strokeRect(850, 70, 350, 20);
+         
+        
+        combat();
 
-        ctx.drawImage(ennemi,935,320,220,300)
+        if(hero.hp>0){
+            //350 = 100*hero.level;
+            //      = hero.hp
+            var pourcent = (hero.hp*350) / (100*hero.level)
 
-        ctx.drawImage(personnage,120,330,200,250)
+            ctx.fillStyle = 'rgb(0, 200, 0)';
+            ctx.fillRect(70, 70, pourcent, 20);
+        }else{
+            ctx.fillStyle = 'rgb(0, 200, 0)';
+            ctx.fillRect(70, 70, 0, 20);
+        }
+           
+        //vie Personnage
+        
 
-        window.setInterval( function() {
-            combat();
+        //Vie Monstre
+       
+
+        if(monstre.hp>0){
+            //350 = 100*hero.level;
+            //      = hero.hp
+            var pourcent = (monstre.hp*350) / (100*hero.level)
+
+            ctx.fillStyle = 'rgb(0, 200, 0)';
+            ctx.fillRect(1200-pourcent, 70, pourcent, 20);
+        }else{
+            ctx.fillStyle = 'rgb(0, 200, 0)';
+            ctx.fillRect(850, 70, 0, 20);
+        }
+
+        
+            
+           
         },1000);
     }
     
